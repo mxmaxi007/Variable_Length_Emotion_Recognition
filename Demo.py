1167f6b75a62ac9c38cde7dd71e7f89966365323
Max Ma <mxmaxi1234@gmail.com>
2018-03-14 20:27:41 +0800

    Demo

--- Demo.py
+++ Demo.py
@@ -21,14 +21,8 @@ import Model.CNN_RNN_Const as CNN_RNN_Const
 import Metrics.Accuracy as Accuracy
 
 
-def main():
-    # if len(sys.argv) != 7:
-    #     print('Usage: python3 ' + sys.argv[0] + ' wav_dir_path spectrogram_dir_path output_dir test_session classifer_type spectrogram_type\n');
-    #     sys.exit(2);
-
-    start = time.time();
-
-    spectrogram_path = "/Users/max/Downloads/Data/Personal/interspeech18/Test_Data/Ses01M_impro01_F019_ang.npy";
+def Var_CNN_Output():
+    spectrogram_path = "/Users/max/Downloads/Data/Personal/interspeech18/Test_Data/Ses01M_impro01_F024_neu.npy";
     model_dir = "/Users/max/Downloads/Data/Personal/interspeech18/CNN_RNN_Var_3/model";
 
     spectrogram = np.load(spectrogram_path);
@@ -42,7 +36,6 @@ def main():
     y_predict = [];
 
     with tf.Session() as sess:
-        info = dict();
         tf.saved_model.loader.load(sess, ["CNN_RNN_Var"], model_dir);
 
         h_pool2 = sess.graph.get_tensor_by_name("pool2/MaxPool:0");
@@ -61,14 +54,77 @@ def main():
     output_max = cnn_out.max();
     cnn_out = 256 * (cnn_out - output_min) / (output_max - output_min);
 
-    input_im = Image.fromarray(spectrogram.T);
-    input_im.show();
+    # input_im = Image.fromarray(spectrogram.T);
+    # input_im.show();
 
-    output_im = Image.fromarray(cnn_out.T);
-    output_im.show();
+    output_im = Image.fromarray(cnn_out.T[::-1, ::-1]);
+    # output_im.show();
+    if output_im.mode != 'RGB':
+        output_im = output_im.convert('RGB');
+
+    output_im.save("/Users/max/Downloads/Data/Personal/interspeech18/Test_Data/cnn_out_var.jpg");
 
 
-        # print(info["probabilities"][0]);
+def Image_Convert():
+    # print(info["probabilities"][0]);
+    spectrogram_img_path = "/Users/max/Downloads/Data/Personal/interspeech18/Test_Data/spectrogram.png";
+    spectrogram_im = Image.open(spectrogram_img_path);
+    spectrogram_im_mat = np.asarray(spectrogram_im.convert("L"));
+    spectrogram_im = Image.fromarray(spectrogram_im_mat[:, ::-1]);
+    spectrogram_im.save("/Users/max/Downloads/Data/Personal/interspeech18/Test_Data/spectrogram.jpg");
+
+
+def Const_CNN_Output():
+    spectrogram_vec_path = "/Users/max/Downloads/Data/Personal/interspeech18/Test_Data/Ses01M_impro01_F019_ang_const.npy";
+    model_dir = "/Users/max/Downloads/Data/Personal/interspeech18/CNN_RNN_Const_3/model";
+
+    spectrogram_vec = np.load(spectrogram_vec_path);
+    spectrogram_vec = spectrogram_vec.reshape(spectrogram_vec.shape[0], spectrogram_vec.shape[1],
+                                              spectrogram_vec.shape[2], 1);
+
+
+    tf.reset_default_graph();
+
+    with tf.Session() as sess:
+        tf.saved_model.loader.load(sess, ["CNN_RNN_Const"], model_dir);
+
+        h_pool2 = sess.graph.get_tensor_by_name("pool2/MaxPool:0");
+
+        cnn_out = sess.run(h_pool2, feed_dict={
+            "inputs:0": spectrogram_vec
+        });
+
+    # print(cnn_out.shape);
+    cnn_out = cnn_out.sum(axis=3);
+    output_min = cnn_out.min();
+    output_max= cnn_out.max();
+    cnn_out = 256 * (cnn_out - output_min) / (output_max - output_min);
+
+    output_im_1 = Image.fromarray(cnn_out[0].T[::-1, ::-1]);
+    # output_im.show();
+    if output_im_1.mode != 'RGB':
+        output_im_1 = output_im_1.convert('RGB');
+
+    output_im_1.save("/Users/max/Downloads/Data/Personal/interspeech18/Test_Data/cnn_out_const_1.jpg");
+
+    output_im_2 = Image.fromarray(cnn_out[1].T[::-1, ::-1]);
+    # output_im.show();
+    if output_im_2.mode != 'RGB':
+        output_im_2 = output_im_2.convert('RGB');
+
+    output_im_2.save("/Users/max/Downloads/Data/Personal/interspeech18/Test_Data/cnn_out_const_2.jpg");
+
+
+def main():
+    # if len(sys.argv) != 7:
+    #     print('Usage: python3 ' + sys.argv[0] + ' wav_dir_path spectrogram_dir_path output_dir test_session classifer_type spectrogram_type\n');
+    #     sys.exit(2);
+
+    start = time.time();
+
+    Var_CNN_Output();
+    # Image_Convert();
+    # Const_CNN_Output();
 
     end = time.time();
     print("Total Time: {}s".format(end - start));
